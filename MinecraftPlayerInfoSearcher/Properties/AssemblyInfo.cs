340bc0d54c770b816d0b24b1ba96943beb4bb099
lkctrl in Cop. <lkctrl@foxmail.com>
2024-07-18 14:09:22 +0800

    x2.0.4_240718_1408 [/] Change Application's name [/] No more clickonce signture, its not required

--- MinecraftPlayerInfoSearcher/Properties/AssemblyInfo.cs
+++ MinecraftPlayerInfoSearcher/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("MinecraftPlayerInfoSearcher")]
+[assembly: AssemblyTitle("MinecraftUsefulApiTools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MinecraftPlayerInfoSearcher")]
+[assembly: AssemblyProduct("MinecraftUsefulApiTools")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
